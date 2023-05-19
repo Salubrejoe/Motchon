24e2a1a645af37a4e5b187538ae01fcdfc2623a4
Lore P <44895517+Salubrejoe@users.noreply.github.com>
2023-05-19 09:39:50 +0100

    Initial Commit

---
+++ Motchon/MotchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotchonApp.swift
+//  Motchon
+//
+//  Created by Lore P on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MotchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
